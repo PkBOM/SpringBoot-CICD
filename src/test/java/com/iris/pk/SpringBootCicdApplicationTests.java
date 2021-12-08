e37f32983b6eaf1afaa17983596d68432d1cd734
Pkbom <pkjavatech@gmail.com>
2021-12-08 18:01:19 +0400

    first commit

---
+++ src/test/java/com/iris/pk/SpringBootCicdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iris.pk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCicdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
